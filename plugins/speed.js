fc72380e44956dcdc3c9cc8bfc0f5c567fbb782c
nanagemoy <142724539+nanagemoy@users.noreply.github.com>
2023-11-28 01:13:00 +0700

    Add files via upload

---
+++ plugins/speed.js
@@ -0,0 +1,42 @@
+const { performance } = require('perf_hooks');
+const { version: nodeVersion, npmVersion } = require('process');
+const { version: packageVersion } = require('../package.json');
+
+let handler = async (m, { conn }) => {
+  const start = performance.now();
+    
+	conn.sendMessage(m.chat, {
+		react: {
+			text: '🕒',
+			key: m.key,
+		}
+	})
+
+  const chatServer = 's.whatsapp.net';
+  const connServer = conn.baseURL?.split('//')[1]?.split(':')[0] || 'Not available';
+  const messageDelay = Math.floor(performance.now() - start);
+
+  const memoryUsage = process.memoryUsage().heapUsed / 1024 / 1024;
+  const cpuUsage = process.cpuUsage().user / 1000000;
+
+  const npmVersionOutput = npmVersion ? `◦ Npm: ${npmVersion}` : '◦ Npm: Not available';
+
+  const reply = `• *Ping!*\n\n• *Network Specs:*\n◦ Chat Server: ${chatServer}\n◦ Connection Server: ${connServer}\n◦ Message Delay: ${messageDelay}ms\n\n• *System Specs:*\n◦ Memory Usage: ${memoryUsage.toFixed(2)}MB\n◦ CPU Usage: ${cpuUsage.toFixed(2)}s\n\n• *Bot Version:*\n◦ Node.js: ${nodeVersion}\n◦ Language: JavaScript\n${npmVersionOutput}\n◦ Version: ${version}\n◦ Owner: Sazumi Kemii`;
+
+  conn.reply(m.chat, reply, m, {
+      contextInfo: {
+        externalAdReply: {
+          title: "Speed Test, 𝐿yosh",
+          thumbnailUrl: 'https://telegra.ph/file/4de8ff391ac0409d61117.jpg',
+          mediaType: 1,
+          renderLargerThumbnail: true
+        }
+      }
+    });
+};
+
+handler.command = ['ping'];
+handler.tags = ['info'];
+handler.help = ['ping'];
+
+module.exports = handler;
